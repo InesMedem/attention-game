334117835fc9c5579d591a677f7f47bc6c06b3af
InesMedem <inesmedem93@gmail.com>
2024-12-10 10:25:50 +1100

    made render work

--- src/render.php
+++ src/render.php
@@ -1,8 +1,17 @@
 <?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
+// Access the block's attributes
+$attributes = isset( $block->attributes ) ? $block->attributes : array();
+
+// Check if the 'question' attribute is set and not empty
+if ( isset( $attributes['question'] ) && ! empty( $attributes['question'] ) ) {
+    echo '<p ' . get_block_wrapper_attributes() . '>';
+    // Display the 'question' attribute
+    echo esc_html( $attributes['question'] );
+    echo '</p>';
+} else {
+    // Fallback message if 'question' is not set or is empty
+    echo '<p ' . get_block_wrapper_attributes() . '>';
+    esc_html_e( 'Please enter a question in the block settings.', 'attention-game' );
+    echo '</p>';
+}
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Attention Game – hello from a dynamic block!', 'attention-game' ); ?>
-</p>
